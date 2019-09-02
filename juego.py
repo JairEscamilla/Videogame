5a26446d9c265f246b3a4a92378b25886a46b614
Jair Escamilla <hernandez_yo14@outlook.es>
2019-09-01 21:20:59 -0500

    Constantes

--- juego.py
+++ juego.py
@@ -1,7 +1,17 @@
 import pygame
 import sys
 
-ventana = pygame.display.set_mode((800, 600))
+# CONSTANTES
+ANCHO = 800
+ALTO = 600
+ROJO = (255, 0, 0)
+
+# Jugador
+jugador_pos = [400, 400]
+jugador_size = 50
+
+# VENTANA
+ventana = pygame.display.set_mode((ANCHO, ALTO))
 
 game_over = False
 
@@ -11,5 +21,5 @@ while not game_over:
 			sys.exit()
 
 	# Dibujando cuadrado
-	pygame.draw.rect(ventana, (255, 0, 0), (400, 400, 50, 50))
+	pygame.draw.rect(ventana, ROJO, (jugador_pos[0], jugador_pos[1], jugador_size, jugador_size))
 	pygame.display.update() 
