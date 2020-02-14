ee5f48fba5e2c4f9025c3207e420fc3bbbcea9e7
Raúl González Portillo <raulgonzalezportillo@gmail.com>
2020-02-14 11:42:13 -0600

    Mejorando aspectos generales del juego: 	-> Se han reducido el numero de carriles disponibles. 	-> Se ha delimitado el tamaño del mapa de juego. 	-> Se ha añadido un sistema de puntuaciones. 	-> Se ha mejorado el framerate de 30 a 60 fps. 	-> Se han mejorado los controles de juego. 	-> Se ha agregado diseño al juego en general.

--- juego.py
+++ juego.py
@@ -4,28 +4,35 @@ import random
 
 # CONSTANTES
 # Dimensiones
-ANCHO = 800
+ANCHO = 300
 ALTO = 600
 # Colores
-ROJO = (255, 0, 0)
-NEGRO = (0, 0, 0)
-AZUL = (0, 0, 255)
+AZUL = (0, 0, 0)
+NEGRO = (255, 255, 255)
+ROJO = (100, 100, 100)
 # Jugador
 jugador_size = 50
 jugador_pos = [ANCHO / 2, ALTO - jugador_size*2]
 
 # Enemigo
 enemigo_size = 50
-enemigo_pos = [random.randint(0, ANCHO - enemigo_size), 0]
+enemigo_pos = [random.randint(0, 5)*50, 0]
 
 
 # VENTANA
 ventana = pygame.display.set_mode((ANCHO, ALTO))
+# PUNTAJE
+puntos = 1
 
 game_over = False
 clock = pygame.time.Clock()
-
-# FUNCION QUE DETECTA COLISIONES	
+# Cargando la imagen
+imagen = pygame.image.load("fondo.png")
+imagen = imagen.convert()
+ventana.blit(imagen, (0, 0))
+pygame.display.flip()
+pygame.key.set_repeat(17)
+# FUNCION QUE DETECTA COLISIONES
 def detectarColision(jugador_pos, enemigo_pos):
 	jx = jugador_pos[0]
 	jy = jugador_pos[1]
@@ -35,36 +42,39 @@ def detectarColision(jugador_pos, enemigo_pos):
 		if (ey >= jy and ey < (jy + jugador_size)) or (jy >= ey and jy < (ey + enemigo_size)):
 			return True
 	return False
-
+counter = 0.015
+dificultad = 200
 while not game_over:
+	puntos = puntos + (dificultad)/100
 	for event in pygame.event.get():
-		if event.type == pygame.QUIT:	
+		if event.type == pygame.QUIT:
 			sys.exit()
 		if event.type == pygame.KEYDOWN:
 			x = jugador_pos[0]
 			if event.key == pygame.K_LEFT:
-				x -= jugador_size
+				if(x >= jugador_size):
+					x -= jugador_size
 			if event.key == pygame.K_RIGHT:
-				x += jugador_size
+				if(x < ANCHO - 50):
+					x += jugador_size
 			jugador_pos[0] = x
 
-	ventana.fill(NEGRO)
-
+	ventana.blit(imagen, (0, 0))
 	if enemigo_pos[1] >= 0 and enemigo_pos[1] < ALTO:
-		enemigo_pos[1] += 20
+		enemigo_pos[1] += counter * dificultad
 	else:
-		enemigo_pos[0] = random.randint(0, ANCHO - enemigo_size)
+		enemigo_pos[0] = random.randint(0, 5) * 50
 		enemigo_pos[1] = 0
-	
+
 	# Colisiones
 	if detectarColision(jugador_pos, enemigo_pos):
 		game_over = True
 
-	# Dibujar enemigo 
+	# Dibujar enemigo
 	pygame.draw.rect(ventana, AZUL, (enemigo_pos[0], enemigo_pos[1], enemigo_size, enemigo_size))
-
+	dificultad = dificultad + 1
 	# Dibujando cuadrado
 	pygame.draw.rect(ventana, ROJO, (jugador_pos[0], jugador_pos[1], jugador_size, jugador_size))
-	clock.tick(30)
-	pygame.display.update() 
-
+	clock.tick(60)
+	pygame.display.update()
+print(int(puntos))
