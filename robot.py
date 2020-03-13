a58408523fd7c67f708055413d1ff5da8e3d9d4f
CesarArellano <raywayday@gmail.com>
2020-03-13 11:14:49 -0600

    Desarrollando clases

--- robot.py
+++ robot.py
@@ -2,60 +2,65 @@ import pygame
 import sys
 import random
 
-# CONSTANTES
-# Dimensiones
-ANCHO = 600
-ALTO = 600
-# Colores
-AZUL = (0, 0, 0)
-NEGRO = (255, 255, 255)
-ROJO = (100, 100, 100)
-# Jugador
-jugador_size = 60
-jugador_pos = [30, 510]
 
 
-# VENTANA
-ventana = pygame.display.set_mode((ANCHO, ALTO))
+class Videojuego():
+	# CONSTANTES
+	# Dimensiones
+	ANCHO = 600
+	ALTO = 600
+	# Colores
+	AZUL = (0, 0, 0)
+	NEGRO = (255, 255, 255)
+	ROJO = (100, 100, 100)
+	def __init__(self):
+		# Jugador
+		self.jugador_size = 60
+		self.jugador_pos = [30, 510]
+		self.game_over = False
+
+		# VENTANA
+		self.ventana = pygame.display.set_mode((self.ANCHO, self.ALTO))
+
+		# Cargando la imagen
+		self.imagen = pygame.image.load("tablero.png")
+		self.imagen = self.imagen.convert()
+		self.ventana.blit(self.imagen, (0, 0))
+		pygame.display.flip()
+	# FUNCION QUE DETECTA COLISIONES
+	def detectarColision(self, jugador_pos, enemigo_pos):
+		jx = jugador_pos[0]
+		jy = jugador_pos[1]
 
 
-game_over = False
-# Cargando la imagen
-imagen = pygame.image.load("tablero.png")
-imagen = imagen.convert()
-ventana.blit(imagen, (0, 0))
-pygame.display.flip()
-# FUNCION QUE DETECTA COLISIONES
-def detectarColision(jugador_pos, enemigo_pos):
-	jx = jugador_pos[0]
-	jy = jugador_pos[1]
+	def startGame(self):
+		while not self.game_over:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					sys.exit()
+				if event.type == pygame.KEYDOWN:
+					x = jugador_pos[0]
+					y = jugador_pos[1]
+					if event.key == pygame.K_LEFT:
+						if(x >= jugador_size):
+							x -= jugador_size * 2
+					if event.key == pygame.K_RIGHT:
+						if(x < ANCHO - 100):
+							x += jugador_size * 2
+					if event.key == pygame.K_UP and y > 30:
+						y -= jugador_size * 2
+					if event.key == pygame.K_DOWN and y < 510:
+						y += jugador_size * 2
+						
+					self.jugador_pos[0] = x
+					self.jself.ugador_pos[1] = y
 
-counter = 0.015
-dificultad = 200
-while not game_over:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			sys.exit()
-		if event.type == pygame.KEYDOWN:
-			x = jugador_pos[0]
-			y = jugador_pos[1]
-			if event.key == pygame.K_LEFT:
-				if(x >= jugador_size):
-					x -= jugador_size * 2
-			if event.key == pygame.K_RIGHT:
-				if(x < ANCHO - 100):
-					x += jugador_size * 2
-			if event.key == pygame.K_UP and y > 30:
-				y -= jugador_size * 2
-			if event.key == pygame.K_DOWN and y < 510:
-				y += jugador_size * 2
-				
-			jugador_pos[0] = x
-			jugador_pos[1] = y
+			self.ventana.blit(self.imagen, (0, 0))
 
-	ventana.blit(imagen, (0, 0))
+			# Dibujando cuadrado
+			pygame.draw.rect(self.ventana, self.ROJO, (self.jugador_pos[0], self.jugador_pos[1], self.jugador_size, self.jugador_size))
+			pygame.display.update()
 
-	# Dibujando cuadrado
-	pygame.draw.rect(ventana, ROJO, (jugador_pos[0], jugador_pos[1], jugador_size, jugador_size))
-	pygame.display.update()
-print(int(puntos))
+if __name__ == '__main__':
+	game = Videojuego()
+	game.startGame()
